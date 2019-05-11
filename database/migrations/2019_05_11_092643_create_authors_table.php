ba4f720ea4e88a650d49d7ec7bc0c56aec62f628
Yan Naing <yannaing@Yans-MacBook-Pro.local>
2019-05-11 16:09:28 +0630

    First commit

---
+++ database/migrations/2019_05_11_092643_create_authors_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('slug', 100);
+            $table->text('bio');
+            $table->string('profile_picture', 200);
+            $table->string('cover_photo', 200);
+            $table->string('website', 150);
+            $table->string('facebook', 150);
+            $table->string('twitter', 150);
+            $table->string('linkded_in', 150);
+            $table->string('youtube', 150);
+            $table->string('instagram', 150);
+            $table->string('github', 150);
+            $table->bigInteger('users_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
