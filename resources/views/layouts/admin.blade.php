fd4cb4ccaf39f92fa527698a7874608fb26af944
Yan Naing <tannaing@gmail.com>
2019-05-15 16:43:27 +0630

    Add AuthorsController and admin layout blade

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('admin/js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+
+    <!-- Styles -->
+    <link href="{{ asset('admin/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        
+    </div>
+</body>
+</html>
