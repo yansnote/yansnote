7f21a994df4de0d680778f9c3a580c303517f68f
Yan Naing <tannaing@gmail.com>
2019-05-16 15:11:39 +0630

    Scaffold articles

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('authors', 'AuthorsController');
+Route::resource('articles', 'ArticlesController')->middleware('auth');
